3bfde668a5cf66be0e8c01231fe40a8346f11004
SINOBNSR <yassine.bnsr@gmail.com>
2023-03-01 12:02:29 -0800

    xtra2 fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,9 +13,8 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
-
 	if ((num / 10 > 0)
 		print_number(num / 10);
 
-		_putchar((num % 10) + '0');
-		}
+	_putchar((num % 10) + '0');
+}
