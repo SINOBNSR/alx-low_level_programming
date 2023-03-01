b4c55baa648588088031eb655a96ea53eec2647b
SINOBNSR <yassine.bnsr@gmail.com>
2023-03-01 11:04:48 -0800

    xtra1 fix 3

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,38 +1,44 @@
 #include "main.h"
 /**
- * *rot - Encodes a string using rot13.
+ * *rot13 - Encodes a string using rot13.
  * @str: the string subject of encoding
  * Return: encoded string
  */
-char *rot(char *str)
+char *rot13(char *str)
 {
 	int inde1 = 0, inde2;
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-		'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-		'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-		'u', 'v', 'w', 'x', 'y', 'z'};
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A',
-		'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-		'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q',
-		'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-		'h', 'i', 'j', 'k', 'l', 'm'};
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
 	while (str[inde1])
 	{
 		for (inde2 = 0; inde2 < 52; inde2++)
 		{
-			if (str[inde1] == alphabet[inde2])
+			if (str[inde1] == alphabet[inde1])
 			{
-				str[inde1] = rotl3key[inde2];
+				str[inde1] = rot13key[inde2];
 				break;
 			}
 		}
+
 		inde1++;
 	}
+
 	return (str);
 }
