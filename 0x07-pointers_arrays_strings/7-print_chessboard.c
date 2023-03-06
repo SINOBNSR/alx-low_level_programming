1065b187f4bb207fe2236da5da1628dd7881f3a8
SINOBNSR <yassine.bnsr@gmail.com>
2023-03-06 14:11:48 -0800

    task6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chessboard pieces
+ * @a: the number lines in the start of the game
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int index1, index2;
+
+	for (index1 = 0; a[index1][7]; index1++)
+	{
+		for (index2 = 0; index2 < 8; index2++)
+			_putchar(a[index1][index2]);
+		_putchar('n');
+	}
+}
