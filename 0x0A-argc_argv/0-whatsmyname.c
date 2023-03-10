51b1ff068fab51678477185df728de674adf0cfe
SINOBNSR <yassine.bnsr@gmail.com>
2023-03-10 11:22:15 -0800

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+include "main.h"
+
+/**
+ * main - prints the name of the programme
+ * followed by a new line.
+ * @argc: Number of arguments
+ * @agrav: Array of pointers to the said arguments
+ *
+ * Return: 0
+ */
+
+int main(int agrc, char *agrv[])
+{
+	printf("%s\n", agrv[agrc * 0]);
+	return (0);
+}
